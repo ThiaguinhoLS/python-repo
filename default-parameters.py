1d2c858dbe57fb87467c8cbe267a5c9f395e686f
ThiaguinhoLS <tthiaguinho638@gmail.com>
2019-03-12 23:36:34 -0300

    Add .gitignore

--- default-parameters.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-def soma(x, y = 3):
-    return x + y
-
-assert soma(1, 3) == 4
-assert soma(1) == 4
-
--- functions.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-
-def soma(x, y):
-    return x + y
-
